0c5f73fa7aabfa54ddee30a5dbd57e338ae77f6a
izra4 <aryaizra2@gmail.com>
2024-09-17 11:35:50 +0700

    feat(server): add method to test server

--- internals/server/run.go
+++ internals/server/run.go
@@ -7,7 +7,9 @@ import (
 
 func Initialize() {
 	app := fiber.New()
-
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 	err := app.Listen(":3000")
 	if err != nil {
 		log.Fatalln("Server error: ", err)
