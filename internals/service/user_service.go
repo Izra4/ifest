a727f6798787b7b8f68dea9cd1fea0b6312fe142
izra4 <aryaizra2@gmail.com>
2024-09-23 11:21:53 +0700

    feat(service): add user service and feat for create user

--- internals/service/user_service.go
+++ internals/service/user_service.go
@@ -1,1 +1,59 @@
 package service
+
+import (
+	"IFEST/helpers"
+	"IFEST/internals/core/domain"
+	"IFEST/internals/repositories"
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+type IUserService interface {
+	Create(user *domain.UserRequest) (domain.User, error)
+	GetByEmail(email string) (domain.User, error)
+}
+
+type UserService struct {
+	userRepository repositories.IUserRepository
+}
+
+func NewUserService(userRepository repositories.IUserRepository) IUserService {
+	return &UserService{
+		userRepository: userRepository,
+	}
+}
+
+func (u *UserService) Create(user *domain.UserRequest) (domain.User, error) {
+	ID := uuid.New()
+
+	_, err := u.userRepository.GetByEmail(user.Email)
+	if err == nil {
+		return domain.User{}, errors.New("user already exist")
+	}
+
+	hashedPass, err := helpers.HashPassword(user.Password)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	userDomain := domain.User{
+		ID:           ID,
+		Name:         user.Name,
+		Email:        user.Email,
+		Password:     hashedPass,
+		IsGoogleAuth: false,
+		CreatedAt:    time.Now(),
+	}
+
+	newUser, err := u.userRepository.Create(&userDomain)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return newUser, nil
+}
+
+func (u *UserService) GetByEmail(email string) (domain.User, error) {
+	return u.userRepository.GetByEmail(email)
+}
