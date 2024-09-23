5a3c7d0a286d3862f1feafd829e305eae8426737
izra4 <aryaizra2@gmail.com>
2024-09-23 10:52:55 +0700

    feat(domain): add user domain

--- internals/core/domain/user_domain.go
+++ internals/core/domain/user_domain.go
@@ -1,7 +1,16 @@
 package domain
 
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"-"`
-	Name     string `json:"name"`
+	ID           uuid.UUID `db:"id" json:"id"`
+	Name         string    `db:"name" json:"name"`
+	Email        string    `db:"email" json:"email"`
+	Password     string    `db:"password" json:"-"`
+	IsGoogleAuth bool      `db:"is_google_auth" json:"isGoogleAuth"`
+	CreatedAt    time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt    time.Time `db:"updated_at" json:"updatedAt"`
 }
