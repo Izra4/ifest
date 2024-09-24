d0e7802f47ba5625997a21edfd905f3699a109af
izra4 <aryaizra2@gmail.com>
2024-09-24 11:18:15 +0700

    feat(domain): add user register and login struct

--- internals/core/domain/user_domain.go
+++ internals/core/domain/user_domain.go
@@ -10,13 +10,18 @@ type User struct {
 	Name         string    `db:"name" json:"name"`
 	Email        string    `db:"email" json:"email"`
 	Password     string    `db:"password" json:"-"`
-	IsGoogleAuth bool      `db:"is_google_auth" json:"isGoogleAuth"`
+	IsGoogleAuth bool      `db:"isgoogleauth" json:"isGoogleAuth"`
 	CreatedAt    time.Time `db:"created_at" json:"createdAt"`
 	UpdatedAt    time.Time `db:"updated_at" json:"updatedAt"`
 }
 
 type UserRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required,min=3"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
 }
---
+++ internals/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"IFEST/internals/handlers"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+type Server struct {
+	userHandler handlers.UserHandler
+}
+
+func NewServer(userHandler handlers.UserHandler) *Server {
+	return &Server{
+		userHandler: userHandler,
+	}
+}
+
+func (s *Server) Initialize() {
+	app := fiber.New()
+
+	user := app.Group("/api/user")
+
+	user.Post("/register", s.userHandler.Create)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!\nTesting the jenkins here")
+	})
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatalln("Server error: ", err)
+	}
+}
