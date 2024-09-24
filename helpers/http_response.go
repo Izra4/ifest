45a10ff2c40d2bccc033bd8ea61d4faf813c68f4
izra4 <aryaizra2@gmail.com>
2024-09-24 11:27:10 +0700

    fix(helpers): fixing return and parameter of HttpResponse

--- helpers/http_response.go
+++ helpers/http_response.go
@@ -1,1 +1,27 @@
 package helpers
+
+import "github.com/gofiber/fiber/v2"
+
+func HttpsInternalServerError(c *fiber.Ctx, msg string, err error) error {
+	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		"status":  "failed",
+		"message": msg,
+		"error":   err.Error(),
+	})
+}
+
+func HttpSuccess(c *fiber.Ctx, msg string, code int, data any) error {
+	return c.Status(code).JSON(fiber.Map{
+		"status":  "success",
+		"message": msg,
+		"data":    data,
+	})
+}
+
+func HttpBadRequest(c *fiber.Ctx, msg string, data ...any) error {
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+		"status":  "failed",
+		"message": msg,
+		"data":    data,
+	})
+}
