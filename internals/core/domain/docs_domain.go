c3e1b4c5b01151caded2a0ae34809c1264b45876
izra4 <aryaizra2@gmail.com>
2024-09-27 18:32:51 +0700

    feat(domain): add docs struct

---
+++ internals/core/domain/docs_domain.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Docs struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+	Name      string    `json:"name" db:"name"`
+	Type      string    `json:"type" db:"type"`
+	Status    int       `json:"status" db:"status"`
+	Number    int       `json:"number" db:"number"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type DocsUpload struct {
+	UserID uuid.UUID `json:"user_id" validate:"required"`
+	Name   string    `json:"name" validate:"required"`
+	Number int       `json:"number" validate:"required"`
+	Type   string    `json:"type" validate:"required"`
+}
