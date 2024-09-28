4880e1b56456e541b7dac9e919a2cfd387ad5f23
izra4 <aryaizra2@gmail.com>
2024-09-28 12:03:10 +0700

    feat(service): add query to create and get user_doc table data

---
+++ internals/services/user_doc_service.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"IFEST/internals/core/domain"
+	"IFEST/internals/repositories"
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+type IUserDocService interface {
+	Create(userID, docID uuid.UUID) (sql.Result, error)
+	FindByUserID(userID uuid.UUID) ([]domain.Docs, error)
+	FindByDocID(docID uuid.UUID) ([]domain.User, error)
+}
+
+type UserDocService struct {
+	userDocRepository repositories.IUserDocRepository
+}
+
+func NewUserDocService(userDocRepository repositories.IUserDocRepository) *UserDocService {
+	return &UserDocService{
+		userDocRepository: userDocRepository,
+	}
+}
+
+func (u *UserDocService) Create(userID, docID uuid.UUID) (sql.Result, error) {
+	return u.userDocRepository.Create(userID, docID)
+}
+
+func (u *UserDocService) FindByUserID(userID uuid.UUID) ([]domain.Docs, error) {
+	return u.userDocRepository.FindByUserID(userID)
+}
+
+func (u *UserDocService) FindByDocID(docID uuid.UUID) ([]domain.User, error) {
+	return u.userDocRepository.FindByDocID(docID)
+}
