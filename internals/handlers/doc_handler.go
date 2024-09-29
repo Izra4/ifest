f6341609f3f695b818654c6d7cf5cb4c6957b23c
izra4 <aryaizra2@gmail.com>
2024-09-29 23:35:23 +0700

    refactor(handler): encrypt number var

--- internals/handlers/doc_handler.go
+++ internals/handlers/doc_handler.go
@@ -6,13 +6,13 @@ import (
 	"IFEST/internals/core/domain"
 	"IFEST/internals/services"
 	"bytes"
+	"encoding/base64"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	storage_go "github.com/supabase-community/storage-go"
 	"io"
 	"log"
-	"strconv"
 )
 
 type DocHandler struct {
@@ -35,13 +35,12 @@ func (dh *DocHandler) Upload(c *fiber.Ctx) error {
 		log.Println(err)
 		return helpers.HttpBadRequest(c, "failed to upload file", nil)
 	}
-	numberStr := c.FormValue("number")
-	number, err := strconv.Atoi(numberStr)
-	if err != nil {
-		return helpers.HttpBadRequest(c, "number must be an integer", err)
-	}
-
+	number := c.FormValue("number")
 	fileType := c.FormValue("type")
+
+	if number == "" || fileType == "" {
+		return helpers.HttpBadRequest(c, "fill the form", nil)
+	}
 
 	src, err := file.Open()
 	if err != nil {
@@ -67,10 +66,13 @@ func (dh *DocHandler) Upload(c *fiber.Ctx) error {
 	}
 	userID, _ := uuid.Parse(userIDStr)
 
+	encryptedNumberBytes, err := helpers.Encrypt([]byte(number))
+	encryptedNumber := base64.StdEncoding.EncodeToString(encryptedNumberBytes)
+
 	docs := domain.DocsUpload{
 		UserID: userID,
 		Name:   fileName,
-		Number: number,
+		Number: encryptedNumber,
 		Type:   fileType,
 	}
 	data, err := dh.docService.Upload(docs)
@@ -102,4 +104,15 @@ func (dh *DocHandler) Download(c *fiber.Ctx) error {
 	c.Set("Content-Disposition", "attachment; filename="+docs.Name)
 	c.Set("Content-Type", "application/octet-stream")
 	return c.Send(decryptedData)
+}
+
+func (dh *DocHandler) GetAll(c *fiber.Ctx) error {
+	userID := c.Locals("userID").(string)
+
+	result, err := dh.docService.FindByUserID(userID)
+	if err != nil {
+		return helpers.HttpNotFound(c, "docs not found")
+	}
+
+	return helpers.HttpSuccess(c, "success to get data", 200, result)
 }
