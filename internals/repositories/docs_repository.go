af40944826def8e799fe0de20ca985a4b402384a
izra4 <aryaizra2@gmail.com>
2024-10-01 14:06:41 +0700

    feat(repository): updating query for find by user id

--- internals/repositories/docs_repository.go
+++ internals/repositories/docs_repository.go
@@ -3,11 +3,12 @@ package repositories
 import (
 	"IFEST/internals/core/domain"
 	"github.com/jmoiron/sqlx"
+	"log"
 )
 
 type IDocsRepository interface {
 	Upload(docs domain.Docs) (domain.Docs, error)
-	FindByID(id string) (domain.Docs, error)
+	FindByID(id string) (domain.DocumentAccessInfo, error)
 	FindByUserID(id string) ([]domain.Docs, error)
 }
 
@@ -41,18 +42,45 @@ func (dr *DocsRepository) Upload(docs domain.Docs) (domain.Docs, error) {
 	return docs, nil
 }
 
-func (dr *DocsRepository) FindByID(id string) (domain.Docs, error) {
+func (dr *DocsRepository) FindByID(id string) (domain.DocumentAccessInfo, error) {
 	query := `
-		SELECT * FROM documents WHERE id = $1
+	SELECT 
+		d.id AS document_id,
+		d.name AS document_name,
+		d.type AS document_type,
+		d.status AS document_status,
+		COUNT(uda.user_id) AS access_count,
+		COALESCE(STRING_AGG(u_access.email, ', '), '') AS access_emails
+	FROM 
+		documents d
+	JOIN 
+		users u_owner ON d.user_id = u_owner.id
+	LEFT JOIN 
+		user_doc_access uda ON d.id = uda.doc_id
+	LEFT JOIN 
+		users u_access ON uda.user_id = u_access.id 
+	WHERE 
+		d.id = $1
+	GROUP BY 
+		d.id, u_owner.email;
+
 	`
-	var data domain.Docs
+	var data domain.DocumentAccessInfo
 	err := dr.db.Get(&data, query, id)
-	return data, err
+	if err != nil {
+		return data, err
+	}
+	return data, nil
 }
 
 func (dr *DocsRepository) FindByUserID(id string) ([]domain.Docs, error) {
+	log.Println(id)
 	query := `
-		SELECT * FROM documents WHERE user_id = $1
+		SELECT documents.*, COUNT(user_doc_access.user_id) AS user_count 
+		FROM documents
+		LEFT JOIN user_doc_access ON documents.id = user_doc_access.doc_id
+		WHERE documents.user_id = $1
+		GROUP BY documents.id
 	`
 	var data []domain.Docs
 	err := dr.db.Select(&data, query, id)
